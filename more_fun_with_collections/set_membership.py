246f2bf11f20be6280c6f85b6ac4cd4801d455e7
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2020-10-16 14:39:26 -0500

    unit tests pass

--- more_fun_with_collections/set_membership.py
+++ more_fun_with_collections/set_membership.py
@@ -6,8 +6,10 @@ Module 8 - sets
 10/16/20
 """
 
-def in_set():
-    pass
+def in_set(set, int):
+    return(int in set)
+
 
 if __name__ == '__main__':
-    print("hi")
+    my_set = {1, 4, 3, 6, 8, 1, 3, 9}
+    print(in_set(my_set, 6))
--- test/test_set_membership.py
+++ test/test_set_membership.py
@@ -2,11 +2,13 @@ import unittest
 from more_fun_with_collections import set_membership as set
 
 class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.test_set = {1, 5, 6, 3, 7, 9}
     def test_in_set_true(self):
-        self.assertTrue(set.in_set())
+        self.assertTrue(set.in_set(self.test_set, 5))
 
     def test_in_set_false(self):
-        self.assertFalse(set.in_set())
+        self.assertFalse(set.in_set(self.test_set, 4))
 
 
 if __name__ == '__main__':
