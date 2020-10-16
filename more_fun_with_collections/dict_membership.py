3569dbf038dc6fade481c79f5921e412eb696ae4
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2020-10-16 14:57:52 -0500

    Unit test passes

--- more_fun_with_collections/dict_membership.py
+++ more_fun_with_collections/dict_membership.py
@@ -8,8 +8,9 @@ program checks a dictionary for the key value given
 """
 
 
-def in_dict():
-    pass
+def in_dict(dict, key):
+    return(key in dict)
+
 
 
 if __name__ == '__main__':
--- test/test_dict_membership.py
+++ test/test_dict_membership.py
@@ -2,11 +2,14 @@ import unittest
 from more_fun_with_collections import dict_membership as dict
 
 class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.test_dict = {'A': 10, 'B': 15, 'C': 30, 'M': 95, 'K': 39}
+
     def test_in_dict_true(self):
-        self.assertTrue(dict.in_dict())
+        self.assertTrue(dict.in_dict(self.test_dict, 'A'))
 
     def test_in_dict_false(self):
-        self.assertFalse(dict.in_dict())
+        self.assertFalse(dict.in_dict(self.test_dict, 'L'))
 
 
 if __name__ == '__main__':
