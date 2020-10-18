afe9d5df6aebe3c2678753a44a945fd093e9a646
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2020-10-17 19:57:28 -0500

    error handling created and updated method to be cleaner and more compact

--- more_fun_with_collections/dict_membership.py
+++ more_fun_with_collections/dict_membership.py
@@ -6,12 +6,50 @@ program checks a dictionary for the key value given
 
 10/16/20
 """
-
+MIN = 0
+MAX = 100
 
 def in_dict(dict, key):
     return(key in dict)
 
 
+def get_test_scores():
+    scores_dict = dict()
+    try:
+        num_score = int(input("How many scores will you be entering? "))
+        if MIN > num_score:
+            raise ValueError
+    except ValueError:
+        raise ValueError
+
+
+    for num in range(num_score):
+        flag = False
+        while not flag:
+            try:
+                score = float(input("Test Score {}: ".format(num + 1)))
+                if MIN <= score <= MAX:
+                    scores_dict.update({str(num + 1): score})
+                    flag = True
+                else:
+                    raise ValueError
+            except ValueError:
+                print("Enter a valid score between {} and {}".format(MIN, MAX))
+
+    return scores_dict
+
+
+def average_scores(scores_dict):
+    total = 0
+    for key in scores_dict:
+        total += scores_dict[key]
+    average = total/len(scores_dict)
+    return average
 
 if __name__ == '__main__':
-    print("")
+    try:
+        my_scores = get_test_scores()
+        print(my_scores)
+        print(average_scores(my_scores))
+    except ValueError:
+        print("Enter an integer")
