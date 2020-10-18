255421983c049a30ade10f819849b48530170060
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2020-10-17 20:15:02 -0500

    Unit tests now pass

--- more_fun_with_collections/assign_average.py
+++ more_fun_with_collections/assign_average.py
@@ -6,7 +6,14 @@
 
 
 def switch_average(entry):
-    pass
+    my_dict = {'A': 'You Entered an A!',
+               'B': 'You Entered an B!',
+               'C': 'You Entered an C!',
+               'D': 'You Entered an D!',
+               'F': 'You Entered an F!'}
+
+    result = my_dict.get(entry, "This is not a valid Grade!")
+    return result
 
 
 if __name__ == '__main__':
