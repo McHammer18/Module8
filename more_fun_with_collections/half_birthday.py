fb695d000bea2808fcb3d44d7d2fb376274c4cfb
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2020-10-17 20:27:03 -0500

    initial

---
+++ more_fun_with_collections/half_birthday.py
@@ -0,0 +1,18 @@
+"""
+Morgan Christensen
+
+Module 8- program that finds a persons half birthday after they sumbit a date
+
+10/17/20
+"""
+
+
+import datetime
+
+
+def half_birthday(datetime):
+    pass
+
+
+if __name__ == '__main__':
+    print((half_birthday()))
---
+++ test/test_half_birthday.py
@@ -0,0 +1,13 @@
+import unittest
+from more_fun_with_collections import half_birthday as half
+from datetime import datetime, timedelta
+class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.date = datetime.now()
+        self.half = self.date + timedelta(days=90)
+    def test_half_birthday(self):
+        self.assertEqual(half.half_birthday(self,datetime), self.half)
+
+
+if __name__ == '__main__':
+    unittest.main()
